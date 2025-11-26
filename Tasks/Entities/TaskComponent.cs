d670289abf3fc418ca8aabe584910be863cfff78
Mateus Lecchi <mateusrlecchi@gmail.com>
2025-11-26 14:39:47 -0300

    Criando componentes com parâmetros

---
+++ Tasks/Entities/TaskComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Tasks.Entities
+{
+    public class TaskComponent
+    {
+        public Guid ID { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public bool Concluida { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
