85cf5f8441ecb0a60f96897fc064ccf224f8e561
Mateus Lecchi <mateusrlecchi@gmail.com>
2025-12-12 15:10:48 -0300

    Criando componentes com Scaffolding. Fazendo algumas mudanças no front.

---
+++ BlazorScaffolding/BlazorScaffolding/Data/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorScaffolding.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string Description { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Price { get; set; } = 0;
+
+        public double Stock { get; set; } = 1;
+
+        public bool Active { get; set; } = true;
+    }
+}
