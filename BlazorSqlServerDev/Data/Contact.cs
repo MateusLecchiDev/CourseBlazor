f269b96c4bd2c2c1a08759fb060be2597cfaf41d
Mateus Lecchi <mateusrlecchi@gmail.com>
2025-12-23 12:05:40 -0300

    feat: configure Blazor Server with Identity, EF Core, SQL Server, and Docker Compose

---
+++ BlazorSqlServerDev/Data/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSqlServerDev.Data
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
